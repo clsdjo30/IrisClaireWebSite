549d2f4566b60892562af179ff1e67fe1ee385db
clsdjo30 <lesergent.cedric@gmail.com>
2023-09-08 06:31:36 +0200

    modify sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://chimerical-caramel-133df7.netlify.app",
+  site: "https://irisclaire.com",
   integrations: [
     tailwind(),
     image({
@@ -18,26 +18,21 @@ export default defineConfig({
     mdx(),
     sitemap({
       customPages: [
-        
-          "https://main--chimerical-caramel-133df7.netlify.app/blog/Travail%20et%20Carri%C3%A8res",
-          "https://main--chimerical-caramel-133df7.netlify.app/blog/Amour",
-          "https://main--chimerical-caramel-133df7.netlify.app/blog/amour/comment-fonctionne-un-homme-au-debut-d-une-relation-bienvenue-dans-la-jungle",
-          "https://main--chimerical-caramel-133df7.netlify.app/blog/amour/triangle-amoureux-comment-s-en-sortir",
-          "https://main--chimerical-caramel-133df7.netlify.app/blog/amour/les-choses-stupides-que-l-on-fait-par-amour-temoignages-epiques",
-          "https://main--chimerical-caramel-133df7.netlify.app/blog/amour/faut-il-etre-amis-avant-d-etre-en-couple",
-          "https://main--chimerical-caramel-133df7.netlify.app/blog/amour/la-peur-de-l-engagement-comment-y-faire-face",
-          "https://main--chimerical-caramel-133df7.netlify.app/blog/amour/l-amour-vache-quand-cupidon-prend-des-vacances-a-la-ferme",
-          "https://main--chimerical-caramel-133df7.netlify.app/blog/amour/amour-a-sens-unique-j-aime-quelqu-un-qui-ne-m-aime-pas",
-          "https://main--chimerical-caramel-133df7.netlify.app/blog/amour/rupture-les-5-phrases-a-bannir",
-          "https://main--chimerical-caramel-133df7.netlify.app/blog/amour/la-jalousie-dans-un-couple-preuve-d-amour-ou-poison",
-          "https://main--chimerical-caramel-133df7.netlify.app/blog/amour/veut-il-vraiment-une-relation-serieuse-les-5-signes-qui-ne-trompent-pas",
-          "https://main--chimerical-caramel-133df7.netlify.app/blog/amour/peut-on-aimer-deux-personnes-a-la-fois-la-quadrature-du-coeur",
-          "https://main--chimerical-caramel-133df7.netlify.app/blog/travail-et-carrieres/la-resilience-au-travail-comment-rebondir-face-aux-defis-professionnels",
-          "https://main--chimerical-caramel-133df7.netlify.app/blog/travail-et-carrieres/le-manager-pervers-narcissique-comment-le-reconnaitre-et-comment-s-en-proteger",
-          "https://main--chimerical-caramel-133df7.netlify.app/blog/travail-et-carrieres/equilibre-entre-vie-professionnelle-et-vie-personnelle-7-conseils-pour-y-parvenir-avec-succes-1",
-          "https://main--chimerical-caramel-133df7.netlify.app/blog/travail-et-carrieres/la-demission-silencieuse-comprendre-et-combattre-le-quiet-quitting",
-          "https://main--chimerical-caramel-133df7.netlify.app/blog/travail-et-carrieres/la-gestion-du-stress-au-travail-strategies-et-conseils"
-        
+        "https://irisclaire.com/blog/Travail%20et%20Carri%C3%A8res",
+        "https://irisclaire.com/blog/Amour",
+        "https://irisclaire.com/blog/Amour/triangle-amoureux-comment-s-en-sortir",
+        "https://irisclaire.com/blog/Amour/faut-il-etre-amis-avant-d-etre-en-couple",
+        // "https://irisclaire.com/blog/amour/comment-fonctionne-un-homme-au-debut-d-une-relation-bienvenue-dans-la-jungle",
+        // "https://irisclaire.com/blog/amour/les-choses-stupides-que-l-on-fait-par-amour-temoignages-epiques",
+        // "https://irisclaire.com/blog/amour/la-peur-de-l-engagement-comment-y-faire-face",
+        // "https://irisclaire.com/blog/amour/l-amour-vache-quand-cupidon-prend-des-vacances-a-la-ferme",
+        // "https://irisclaire.com/blog/amour/amour-a-sens-unique-j-aime-quelqu-un-qui-ne-m-aime-pas",
+        // "https://irisclaire.com/blog/amour/rupture-les-5-phrases-a-bannir",
+        // "https://irisclaire.com/blog/amour/la-jalousie-dans-un-couple-preuve-d-amour-ou-poison",
+        // "https://irisclaire.com/blog/amour/veut-il-vraiment-une-relation-serieuse-les-5-signes-qui-ne-trompent-pas",
+        // "https://irisclaire.com/blog/amour/peut-on-aimer-deux-personnes-a-la-fois-la-quadrature-du-coeur",
+        "https://irisclaire.com/blog/Travail",
+        "https://irisclaire.com/blog/Travail/la-generation-z-au-travail-comprendre-motiver-et-integrer-la-nouvelle-force-du-monde-professionnel",
       ]
     }),
     react(),
