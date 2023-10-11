a5a3b8ee39cd04be381ef11e40ac2abc4feaf50e
clsdjo30 <lesergent.cedric@gmail.com>
2023-10-11 11:46:36 +0200

    add sitempa url

--- src/requests/apiClient.ts
+++ src/requests/apiClient.ts
@@ -9,5 +9,11 @@ export async function fetchPosts() {
     const response = await fetch(`https://iris-api.candcom.com/api/categories?populate=*`);
     const data = await response.json();
     return data.data;
-  }
+}
+  
+export async function fetchCardSignification() {
+    const response = await fetch(`https://iris-api.candcom.com/api/cartes?populate=*`);
+    const data = await response.json();
+    return data.data;
+}
   
